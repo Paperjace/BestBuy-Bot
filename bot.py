8f0ebb48874d461f00c60e5efe4ad8f85a1afa58
Jason Hurley <paperjace@gmail.com>
2025-02-19 19:42:57 -0500

    Added more logging.  Automatically clicks the 'Delivery' radio button instead of default in-store pickup.  Also changes item quantity to one.  Still has bugs.  WIP

--- bot.py
+++ bot.py
@@ -1,6 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support.ui import WebDriverWait, Select
 from selenium.webdriver.support import expected_conditions as EC
 
 import info
@@ -27,11 +27,12 @@ driver.get(info.RTXLINK1)
 
 isComplete = False
 
-notify()
-input("Press Enter to continue...")
+# notify()
+# input("Press Enter to continue...")
 
 while not isComplete:
     # find add to cart button
+    print("Looking for 'Add to Cart' button...")
     try:
         atcBtn = WebDriverWait(driver, 6).until(
             EC.element_to_be_clickable((By.CSS_SELECTOR, ".add-to-cart-button"))
@@ -46,22 +47,48 @@ while not isComplete:
         # add to cart
         atcBtn.click()
 
-        time.sleep(100)
+        time.sleep(3)
 
         # go to cart and begin checkout as guest
         driver.get("https://www.bestbuy.com/cart")
+
+        # Find and click the delivery button instead of Pickup
+        deliveryBtn = WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.CSS_SELECTOR, 'input[id^="fulfillment-shipping"]'))
+        )
+
+        deliveryBtn.click()
+        time.sleep(3)
+        
+        dropdownElement = WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.CSS_SELECTOR, 'select[id^="quantity-"]'))
+        )
+        dropdown = Select(dropdownElement)
+        
+        dropdown.select_by_visible_text('1')
+
+        # Wait for page reload when changing quantity
+        time.sleep(3)
+        
+        if dropdown.first_selected_option.text != '1':
+            print(dropdown.first_selected_option)
+            raise ValueError("The quantity is not 1")
+        
+        print("Successfully added to cart - beginning check out")
 
         checkoutBtn = WebDriverWait(driver, 10).until(
              EC.presence_of_element_located((By.XPATH, "/html/body/div[1]/main/div/div[2]/div/div[1]/div/div[1]/div[1]/section[2]/div/div/div[3]/div/div[1]/button"))
         )
         checkoutBtn.click()
-        print("Successfully added to cart - beginning check out")
-
-        #I will try and be logged-in in advance
+        # continuePaymentInfo = WebDriverWait(driver, 10).until(
+        #      EC.presence_of_element_located((By.XPATH, "/html/body/div[1]/div[2]/div/div[2]/div[1]/div[1]/main/div[3]/div[1]/section/div[2]/section/div/div/button"))
+        # )
+        # continuePaymentInfo.click()
+        # print("Successfully added to cart - beginning check out")
 
         # fill in card cvv
         cvvField = WebDriverWait(driver, 10).until(
-            EC.presence_of_element_located((By.CSS_SELECTOR, ".summary-tile__cvv-code-input"))
+            EC.presence_of_element_located((By.ID, "cvv"))
         )
         cvvField.send_keys(info.cvv)
         
@@ -69,9 +96,11 @@ while not isComplete:
 
         # place order
         placeOrderBtn = WebDriverWait(driver, 10).until(
-            EC.presence_of_element_located((By.XPATH, "/html/body/div[1]/div[2]/div/div[2]/div[1]/div[1]/main/div[3]/div[1]/div/div[4]/section/div/div/div[2]/div/button"))
+            EC.presence_of_element_located((By.XPATH, "/html/body/div[1]/div[2]/div/main/div[3]/div[1]/div[6]/div/div/div/div/div[2]/button"))
         )
-        placeOrderBtn.click()
+        # placeOrderBtn.click()
+        print("Is place order button found?")
+        print(placeOrderBtn.is_displayed())
 
         WebDriverWait(driver, 120).until(
             EC.presence_of_element_located((By.CSS_SELECTOR, ".thank-you-enhancement__info"))
